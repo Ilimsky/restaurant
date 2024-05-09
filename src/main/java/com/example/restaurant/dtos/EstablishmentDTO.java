7a267e9c12deaefe16097327e06a314fba583d94
Ilimsky <ilim.sarykbaev@gmail.com>
2024-05-09 14:58:50 +0600

    Added dtos

---
+++ src/main/java/com/example/restaurant/dtos/EstablishmentDTO.java
@@ -0,0 +1,17 @@
+package com.example.restaurant.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class EstablishmentDTO {
+
+    private String name;
+
+    private String description;
+}
---
+++ src/main/java/com/example/restaurant/dtos/ReviewDTO.java
@@ -0,0 +1,17 @@
+package com.example.restaurant.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ReviewDTO {
+
+    private int rating;
+
+    private String comment;
+}

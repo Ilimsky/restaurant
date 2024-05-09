843cf8b832a183606f31b9e1293caf846a86b13f
Ilimsky <ilim.sarykbaev@gmail.com>
2024-05-09 15:10:37 +0600

    Added repos

---
+++ src/main/java/com/example/restaurant/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.restaurant.repository;
+
+import com.example.restaurant.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, User> {
+}

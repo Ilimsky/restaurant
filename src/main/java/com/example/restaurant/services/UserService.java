f10a84ca53f142efdae753d639307f51729c988c
Ilimsky <ilim.sarykbaev@gmail.com>
2024-05-09 18:07:51 +0600

    Added UserService and his realization

---
+++ src/main/java/com/example/restaurant/services/UserService.java
@@ -0,0 +1,7 @@
+package com.example.restaurant.services;
+
+import com.example.restaurant.entities.User;
+
+public interface UserService {
+    void register(User user);
+}
---
+++ src/main/java/com/example/restaurant/services/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.restaurant.services;
+
+import com.example.restaurant.entities.User;
+import com.example.restaurant.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByEmail(email);
+        user.orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+        return user.get();
+    }
+
+
+    @Override
+    public void register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+}
